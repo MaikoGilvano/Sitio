4f90ac4bc57935fd8e56e2ec244d57872ca0314e
MAIKO <MAIKO@MAIKO>
2016-05-01 22:12:58 -0300

    login

---
+++ Sitio1.0/src/View/WindowLogin.java
@@ -0,0 +1,10 @@
+package View;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class WindowLogin extends JFrame{
+	private JPanel painel;
+	private JTextField texto;
+}
