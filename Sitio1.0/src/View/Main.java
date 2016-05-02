ed0bc78d77b5db41317bfe112777ced1360a7310
MAIKO <MAIKO@MAIKO>
2016-05-01 22:07:15 -0300

    Teste 1

---
+++ Sitio1.0/src/View/Main.java
@@ -0,0 +1,14 @@
+package View;
+
+import Model.Usuario;
+
+public class Main {
+
+	public static void main(String [ ] args){
+		Usuario user = new Usuario();
+		user.setNome("Maiko");
+		System.out.println(user.getNome());
+		
+	}
+	
+}
